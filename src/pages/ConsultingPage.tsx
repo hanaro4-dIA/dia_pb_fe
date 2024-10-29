55d086b204e6a98d246fac1c730a6d6b8bb7b93f
Joe <kavvy99@naver.com>
2024-10-29 17:06:26 +0900

    Feature/make journals (#39)
    * [feat : makeJournals] : 상담요청 내용에 대한 데이터 입력
    
    * [feat : makeJournals] : 첨부파일 입력 기능 추가
    
    * [feat : makeJournals] : 도큐먼트, 스크립트 입력칸 배치

--- src/pages/ConsultingPage.tsx
+++ src/pages/ConsultingPage.tsx
@@ -1,54 +1,3 @@
-// import ConsultationJournalList from '../components/ConsultationJournalList';
-// import CustomerInformation from '../components/CustomerInformation';
-// import MakeJournal from '../components/MakeJournal';
-// import S from '../components/Stt';
-// import { Button } from '../components/ui/button';
-
-// export default function ConsultingPage() {
-//   return (
-//     <>
-//       <div className='flex items-start justify-center w-full h-screen p-5 space-x-4 overflow-hidden'>
-//         {/* 프로필과 들어온 상담 요청 */}
-//         <div className='flex flex-col flex-grow w-1/4 h-full space-y-4'>
-//           <div className='flex justify-between p-3 items-center border-b border-black'>
-//             <div className='text-2xl font-bold text-hanagold'>조경은 손님</div>
-//             <div>
-//               <Button className='border border-hanaindigo bg-white text-black hover:text-white'>
-//                 전화
-//               </Button>
-//             </div>
-//           </div>
-
-//           {/* 손님 정보 */}
-//           <div className='overflow-y-auto min-h-[300px]'>
-//             <CustomerInformation customerId={1} />
-//           </div>
-
-//           {/* 상담일지 리스트 */}
-//           <div className='overflow-y-auto'>
-//             <ConsultationJournalList customerId={1} />
-//           </div>
-//         </div>
-
-//         {/* STT 자동 작성란 */}
-//         <div className='flex flex-col flex-grow w-1/4 h-full'>
-//           <div className='overflow-y-auto'>
-//             <S />
-//           </div>
-//         </div>
-
-//         {/* 상담일지 작성하기 */}
-//         <div className='flex flex-col flex-grow w-1/4 h-full'>
-//           <div className='overflow-y-auto'>
-//             <MakeJournal />
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ConsultationJournalList from '../components/ConsultationJournalList';
@@ -124,9 +73,7 @@ export default function ConsultingPage() {
 
         {/* 상담일지 작성하기 */}
         <div className='flex flex-col flex-grow w-1/4 h-full'>
-          <div className='overflow-y-auto'>
-            <MakeJournal />
-          </div>
+          <MakeJournal />
         </div>
       </div>
     </>
