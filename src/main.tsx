747f1a630545fe44842e78286e9e7d717d7e125d
Hana_JaeJun <wowns0912@naver.com>
2024-11-19 16:09:47 +0900

    [feat] : NavigationBtn(네비게이션 버튼) 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { StrictMode } from 'react';
 import App from './App.tsx';
+import NavigationBtn from './components/NavigationBtn.tsx'
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <NavigationBtn/>
       <App />
     </BrowserRouter>
   </StrictMode>
