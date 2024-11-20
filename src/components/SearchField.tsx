99641deb7212e08c1ebd90c3ae610c3daeca3dc7
Kyoungeun Jo <kavvy99@naver.com>
2024-11-20 17:39:43 +0900

    [style] : SearchField focus 시 input 테두리 색상 변경

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -15,7 +15,7 @@ export const SearchField = ({ placeholder, value, onChange }: TSearchField) => {
           placeholder={placeholder}
           value={value}
           onChange={(e) => onChange(e.target.value)}
-          className='w-full h-[2.5rem] bg-white/60 rounded-lg border border-hanaindigo pl-4'
+          className='w-full h-[2.5rem] bg-white/60 rounded-lg border border-hanaindigo pl-4 focus:ring-2 focus:ring-inset focus:ring-hanasilver focus:outline-none'
         />
         <IoMdSearch className='absolute right-4 top-1/2 transform -translate-y-1/2 text-hanaindigo' />
       </div>
