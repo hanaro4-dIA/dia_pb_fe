c6278c74051af055a4587b713260411098c1ce50
Joe <kavvy99@naver.com>
2024-11-26 17:19:44 +0900

    [fix] : ConsultationJournalListItem 내 상담일지 제목과 일시 겹침 문제 해결

--- src/components/ConsultationJournalListItem.tsx
+++ src/components/ConsultationJournalListItem.tsx
@@ -17,16 +17,18 @@ export const ConsultationJournalListItem = ({
       className='bg-white rounded-lg p-4 mb-4 shadow-lg flex items-center border justify-around border-gray-200 cursor-pointer'
     >
       <div className='w-full flex items-center justify-between'>
-        <div className='flex space-x-2 items-center'>
+        <div className='w-2/3 flex justify-between items-center'>
           <span className='text-hanaindigo font-bold mr-4'>{index + 1}</span>
 
-          <div>
+          <div className='w-[80%] flex flex-col justify-between'>
             <div className='font-bold truncate'>{consultation.content}</div>
             <span className='font-light'>{consultation.pbName} PB</span>
           </div>
         </div>
 
-        <span className='font-light'>{consultation.date}</span>
+        <span className='w-1/3 font-light flex justify-end items-center'>
+          {consultation.date}
+        </span>
       </div>
     </div>
   );
