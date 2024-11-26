89b2e463c31371b963cf8e5902623812d21097e0
Hana_JaeJun <wowns0912@naver.com>
2024-11-26 16:53:04 +0900

    [feat] : 들어온 상담 요청 관련 컴포넌트 분리 및 빠른 상담 요청 효과 추가

--- src/containers/ConsultationRequest.tsx
+++ src/containers/ConsultationRequest.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Section from '../components/Section';
 import { type TRequestedConsultationsProps } from '../lib/types';
+import { RequestedConsultaionItem } from '../components/RequestedConsultaionItem';
 
 type TConsultationRequestProps = {
   onApprove: (consultation: TRequestedConsultationsProps) => void;
@@ -19,17 +20,21 @@ export default function ConsultationRequest({
         const response = await fetch('/data/Consultings.json');
         const data: TRequestedConsultationsProps[] = await response.json();
 
-        // 필터 및 정렬 조건: approvalStatus가 false이면서 finishStatus가 false인 항목만, requestDay 기준으로 오름차순 정렬
+        // 필터 및 정렬 조건: approvalStatus가 false이면서 finishStatus가 false인 항목만
+        // quick이 true인 항목을 우선 정렬하고, 그 다음 requestDay 기준으로 오름차순 정렬
         const filteredData = data
           .filter(
             ({ approvalStatus, finishStatus }) =>
               approvalStatus === false && finishStatus === false
           )
-          .sort(
-            (a, b) =>
+          .sort((a, b) => {
+            if (a.quick && !b.quick) return -1;
+            if (!a.quick && b.quick) return 1;
+            return (
               new Date(a.requestDay).getTime() -
               new Date(b.requestDay).getTime()
-          );
+            );
+          });
 
         setConsultationData(filteredData);
       } catch (error) {
@@ -59,47 +64,14 @@ export default function ConsultationRequest({
       <Section title='들어온 상담 요청'>
         <div className='w-full h-fit p-4'>
           {consultationData.length > 0 ? (
-            consultationData.map(
-              ({
-                id,
-                name,
-                title,
-                hopeDay,
-                hopeTime,
-                requestDay,
-                approvalStatus,
-              }) => (
-                <article
-                  key={id}
-                  className='w-full flex flex-col justify-end mb-4 items-start'
-                >
-                  <small className='ml-2'>{requestDay}</small>
-                  <div className='bg-white rounded-lg border border-gray-200 p-4 shadow-lg w-full'>
-                    <div className='flex justify-between items-center'>
-                      <div className='flex flex-col w-[70%]'>
-                        <span className='w-[95%] text-[1rem] font-bold truncate	'>
-                          {name} 손님
-                        </span>
-                        <span className='w-[95%] font-bold truncate	'>
-                          {title}
-                        </span>
-                        <span className='text-[0.8rem] font-bold'>
-                          요청일: {hopeDay} {hopeTime}
-                        </span>
-                      </div>
-
-                      <button
-                        className='w-[6rem] h-[2rem] text-white text-[1rem] rounded-lg bg-hanaindigo hover:bg-hanagold'
-                        onClick={() => toggleApprovalStatus(id)}
-                      >
-                        {!approvalStatus && '승인대기'}
-                      </button>
-                    </div>
-                  </div>
-                </article>
-              )
-            )
-          ) : (
+          consultationData.map((consultation) => (
+            <RequestedConsultaionItem
+              key={consultation.id}
+              {...consultation}
+              onApprove={toggleApprovalStatus}
+            />
+          ))
+        ) : (
             <div className='text-center text-hanaindigo p-4 h-full text-xl border-x border-b border-gray-200'>
               모든 상담 요청이 승인되었습니다.
             </div>
