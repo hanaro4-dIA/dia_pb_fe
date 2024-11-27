0c08ee31e1057780a8382cce9df687f37221581e
Joe <kavvy99@naver.com>
2024-11-27 11:10:41 +0900

    [docs] : shandcn 컴포넌트 사용으로 생성된 cn(util)의 파일명 변경 design-util

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { cn } from '../../utils/utils';
+import { cn } from '../../utils/design-util';
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
