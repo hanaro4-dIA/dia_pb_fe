c746188b9aed60ab7358659868b777135f9aa345
Joe <kavvy99@naver.com>
2024-11-27 16:39:21 +0900

    [feat] : 로그인 여부에 따른 페이지 접근 제한

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import NavigationBtn from './components/NavigationBtn';
 import ConsultingPage from './pages/ConsultingPage';
 import CustomerDetailPage from './pages/CustomerDetailPage';
 import DictionaryPage from './pages/DictionaryPage';
@@ -7,17 +7,33 @@ import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
 import NotificationPage from './pages/NotificationPage';
 
-// session 작업 필요
 function App() {
+  // session 작업 필요
+  // const [userInfo] = useRecoilState(userInfoState);
+  // const isLogin = userInfo.isLogin;
+  const isLogin = false;
+
   return (
-    <Routes>
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/' element={<MainPage />} />
-      <Route path='/customerDetail/:id' element={<CustomerDetailPage />} />
-      <Route path='/consulting/:id' element={<ConsultingPage />} />
-      <Route path='/dictionary' element={<DictionaryPage />} />
-      <Route path='/notification' element={<NotificationPage />} />
-    </Routes>
+    <BrowserRouter>
+      <NavigationBtn />
+      <Routes>
+        <Route path='/login' element={<LoginPage />} />
+        {isLogin && (
+          <>
+            <Route path='/' element={<MainPage />} />
+            <Route
+              path='/customerDetail/:id'
+              element={<CustomerDetailPage />}
+            />
+            <Route path='/consulting/:id' element={<ConsultingPage />} />
+            <Route path='/dictionary' element={<DictionaryPage />} />
+            <Route path='/notification' element={<NotificationPage />} />
+          </>
+        )}
+        {/* 로그인 안 했는데 접근이 불가능한 페이지에 접속한 경우 */}
+        <Route path='*' element={<Navigate to='/login' />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { StrictMode } from 'react';
 import App from './App.tsx';
-import NavigationBtn from './components/NavigationBtn.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <NavigationBtn />
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
