0df98b20ec47e5ecfccc06b4973ed844469c246b
Joe <kavvy99@naver.com>
2024-12-21 23:24:07 +0900

    [fix] : ConsultationProvider 감싸주는 범위 MainPage -> main.tsx로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import App from './App.tsx';
+import { ConsultationProvider } from './hooks/consultationsContext.tsx';
 import { SessionProvider } from './hooks/sessionContext.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <SessionProvider>
-      <App />
+      <ConsultationProvider>
+        <App />
+      </ConsultationProvider>
     </SessionProvider>
   </StrictMode>
 );
