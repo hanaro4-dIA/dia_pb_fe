8737ceeea39b2f8771403469af17a734a06f603b
Hana_JaeJun <wowns0912@naver.com>
2024-12-23 11:02:07 +0900

    [feat] : 사진 변경 추가

--- src/containers/PbProfile.tsx
+++ src/containers/PbProfile.tsx
@@ -7,7 +7,7 @@ import { type TPbDataProps } from '../types/dataTypes';
 const APIKEY = import.meta.env.VITE_API_KEY;
 
 export default function PbProfile() {
-  const { data, error, fetchData } = useFetch<TPbDataProps>('pb/profile');
+  const { data, error, fetchData } = useFetch<TPbDataProps>('profile');
   const [pbData, setPbData] = useState<TPbDataProps | null>(null);
   const [isEditing, setIsEditing] = useState(false);
   const [isAvailable, setIsAvailable] = useState(false);
@@ -85,14 +85,18 @@ export default function PbProfile() {
     if (fileInput.current?.files?.[0]) {
       formData.append('file', fileInput.current.files[0]);
     }
+
     formData.append('introduce', introduce || '');
+
     hashtagList.forEach(tag => formData.append('hashtags', tag));
 
     try {
-      await fetch(`${APIKEY}/pb/profile`, {
+      await fetch(`${APIKEY}profile`, {
         method: 'PUT',
         body: formData,
+        credentials: 'include',
       });
+      
       // 리팩토링 PUT에 대한 커스텀훅을 써야함(위에서 GET으로 이미 받는데 어떻게 해야하나 고민..)
       setIsEditing(false);
     }
@@ -101,7 +105,6 @@ export default function PbProfile() {
     }
   };
 
-  // 사진쪽도 url로 하는지, 업로드로 하는지 의문.
   const handleImage = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
